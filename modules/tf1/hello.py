cc0ad5483fe6f171db25fb310c5c50c5a02e96ba
zhmhbest <zhmhbest@gmail.com>
2021-01-06 15:30:49 +0800

    update

--- modules/tf1/hello.py
+++ modules/tf1/hello.py
@@ -1,9 +1,11 @@
-import tensorflow.compat.v1 as tf
+# import tensorflow.compat.v1 as tf
+import tensorflow._api.v2.compat.v1 as tf
 tf.disable_v2_behavior()
 
 a = tf.constant(1)
 b = tf.constant(2)
 c = tf.add(a, b)
+print(c)
 
 with tf.Session() as sess:
     r = sess.run(c)
---
+++ modules/torch/hello.py
@@ -0,0 +1,7 @@
+import torch
+
+a = torch.tensor(1, dtype=torch.float32)
+b = torch.tensor(2, dtype=torch.float32)
+c = a + b
+
+print(c)
