24f49e6afd3fc2c08e87bf2e1e832a986b5fd895
zhmhbest <zhmhbest@gmail.com>
2021-02-23 12:05:30 +0800

    update

--- cmdPackagesInstall.sh
+++ cmdPackagesInstall.sh
@@ -1,2 +1,5 @@
-source "`pwd`/venv/Scripts/activate"
-pip install -r "requirements.txt" --no-index -f packages
+if [ -d venv ] && [ -d packages ]; then
+    source "`pwd`/venv/Scripts/activate"
+    # whereis pip
+    pip install -r "requirements.txt" --no-index -f packages
+fi
