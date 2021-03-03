0dc41ddc204bda9cc98ca4db93795a5a623105f6
zhmhbest <zhmhbest@gmail.com>
2021-03-03 11:20:30 +0800

    update

--- modules/HelloPytorch/attention/__init__.py
+++
@@ -1,6 +0,0 @@
-
-from .base import LayerNorm, clones, subsequent_mask
-from .architecture import Encoder, Decoder, EncoderDecoder
-from .core import MultiHeadedAttention, PositionWiseFeedForward
-from .embed import PositionalEncoding, Embeddings
-from .main import EncoderLayer, DecoderLayer, Transformer
---
+++ modules/HelloPytorch/attention/common_header.py
@@ -0,0 +1,8 @@
+import math
+from copy import deepcopy
+import numpy as np
+import torch
+from torch import Tensor, BoolTensor
+from torch import nn
+from torch.nn import functional as F
+# from torch.autograd import Variable
