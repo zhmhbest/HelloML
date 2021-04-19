67ce31b7969b462cada4579f6a51af7901568045
zhmhbest <zhmhbest@gmail.com>
2021-04-19 10:03:14 +0800

    update

--- cmdPackagesDownload.sh
+++ cmdPackagesDownload.sh
@@ -1,9 +1,10 @@
 PYTORCH_LIST='https://download.pytorch.org/whl/torch_stable.html'
 # PYPI_LIST='https://pypi.org/simple/'
-# PYPI_LIST='https://pypi.tuna.tsinghua.edu.cn/simple'
-PYPI_LIST='https://mirrors.aliyun.com/pypi/simple'
+PYPI_LIST='https://pypi.tuna.tsinghua.edu.cn/simple'
+# PYPI_LIST='https://mirrors.aliyun.com/pypi/simple'
 
 if [ -d venv ]; then
     source "`pwd`/venv/Scripts/activate"
-    pip download -r "requirements.txt" -d "packages" -f "$PYTORCH_LIST" -i "$PYPI_LIST"
 fi
+
+pip download -r "requirements.txt" -d "packages" -f "$PYTORCH_LIST" -i "$PYPI_LIST"
